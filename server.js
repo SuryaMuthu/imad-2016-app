c741ef6f6ff1561f2db9a41b403d2269e4e49420
SuryaMuthu <rainbow.muthu8@gmail.com>
2016-10-07 09:29:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content= {
+    title: 'Article-One|Surya Muthukaruppaiah',
+    heading:'Article-One'
+    date:'SEP 13,2016'
+    content:    `<p>
+               hi this is surya
+               
+           </p>
+           <p>
+               
+               hi this is surya
+           </p>
+           <p>
+               hi this is surya
+           </p>`
+    };
+    
+function createTemplate(data){
+    
+    var title = data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+    
+    
+    
+    
+    
+var htmlTemplate=
+    
+    `<html>
+    <head>
+   <title>
+          ${title};
+        
+   </title>
+   </head>
+   <body>
+       <div>
+          <a href="/">Home</a>    
+       </div>
+       <hr/>
+       <h3>
+           ${heading};
+       </h3>
+       <div>
+           
+          ${date};
+       </div>
+       <div>
+           ${content};
+           
+          </div>
+      </body>
+</html>`
+    ;
+    return htmlTemplate;
+    
+ 
+    
+}
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -15,7 +83,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/article-one',function(req,res){
     
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
+     res.send(createTemplate(articleOne)));
     
 });
 app.get('/article-two',function(req,res){
